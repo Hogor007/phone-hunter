8e73c7ec01d8d5c246ea5c6bb81c57346cf8909b
Hogor007 <asifpenta@gmail.com>
2022-03-02 20:50:32 +0600

    added spinner

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// random comment
+//Update the innerHtml
+const toDoData = data => {
+    displayLoading()
+    //Making an array of all the data
+    const arrayOfData = data.data.map(e => {
+        return e;
+    })
+    //Getting the first 20 elements of the array
+    const zeroToTwenty = arrayOfData.slice(0, 20);
+    //Shows 20 phones as default
+    showTotalData(zeroToTwenty);
+    hideLoading()
+}
