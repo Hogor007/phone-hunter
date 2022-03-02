0b21319d9b46d7660ee96f3463ea6cdbae76c599
Hogor007 <asifpenta@gmail.com>
2022-03-02 20:56:54 +0600

    products list

--- script.js
+++ script.js
@@ -11,3 +11,52 @@ const toDoData = data => {
     showTotalData(zeroToTwenty);
     hideLoading()
 }
+//Updates total data fetched for first 20 and show more button
+const showTotalData = array => {
+    const cardParent = document.getElementById('parent-card');
+    array.forEach(e => {
+        // console.log(e.slug)
+        const singleCard = document.createElement('div');
+        singleCard.classList.add('col-12', 'col-md-4', 'pt-3')
+        singleCard.innerHTML = `
+              <div class="card text-center">
+                  <img src="${e.image}" class="card-img-top w-50 mx-auto pt-4" alt="...">
+                  <div class="card-body">
+                      <h5 class="card-title">${e.phone_name}</h5>
+                      <p class="card-text">Brand: ${e.brand}</p>
+                      <a href="#" onclick = "explore('${e.slug}')" id="explore-btn" class="btn btn-primary">Explore</a>
+                  </div>
+              </div>
+          `
+        cardParent.appendChild(singleCard)
+    })
+}
+
+//Search button functionality
+const button = document.getElementById('search-btn');
+button.addEventListener('click', () => {
+    commonForHome();
+});
+
+//Logo and Default products common function
+const commonForHome = () => {
+    displayLoading();
+    const searchField = document.getElementById('input');
+    const searchText = searchField.value;
+    //Clears previous search
+    const cardParent = document.getElementById('parent-card');
+    const detailParent = document.getElementById('parent-detail');
+    cardParent.innerHTML = '';
+    detailParent.innerHTML = '';
+    searchField.value = '';
+    //Making the URL dynamic
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            updateBySearch(data);
+            coountSearchResult(data);
+            updateByShowMore(data);
+            displayShowMore(data)
+        })
+}
